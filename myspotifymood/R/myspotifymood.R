a9bb30162dae22557d65741ce0647fe8b7468656
Alexa <popealexa00@gmail.com>
2021-04-22 14:33:54 -0400

    Add README
    Add the README markdown in files (unfinished)

--- myspotifymood/R/myspotifymood.R
+++ myspotifymood/R/myspotifymood.R
@@ -1,37 +1,21 @@
 #' Title
-
 #'
-
 #' Description
-
 #'
-
 #' Details
-
 #'
-
 #' @author Kurt Wirth
-
 #'
-
 #' @param x A number.
-
+#'
 #' @param y A number.
-
 #'
-
 #' @return The sum of \code{x}^2 and \code{y}^2
-
 #'
-
 #' @examples
-
 #' add_squares(1, 2)
-
 #' add_squares(1, -2)
-
 #'
-
 #' @export
 
 # ESTABLISH CONNECTION SPOTIFY API
