8f3fb63fc2dd84d3d109ca6a102eb9f336aab1af
Alexa <popealexa00@gmail.com>
2021-05-03 11:32:36 -0400

    Finalizing Changes
    Finalizing edits to Description and README

--- myspotifymood/R/myspotifymood.R
+++ myspotifymood/R/myspotifymood.R
@@ -1,68 +1,50 @@
-#' Title
+#' Visualizes Valence From Spotify
 #'
-#' Description
+#' A function that allows the user to retrieve the individual tracks valance
+#' (happiness level) on a specific playlist.
 #'
-#' Details
+#' @author Alexa Pope
 #'
-#' @author Kurt Wirth
+#' @param username The Spotify username of the individual utilizing this
+#' function.
 #'
-#' @param x A number.
+#' @param  client_id The user's Spotify Developer Client ID
 #'
-#' @param y A number.
+#' @param  client_secret The user's Spotify Developer Client Secret.
 #'
-#' @return The sum of \code{x}^2 and \code{y}^2
+#' @param  playlists The user's chosen playlist's id.
+#'
+#' @return A visualization of the valence levels of every track on a specific
+#' playlist of the user's choosing.
 #'
 #' @examples
-#' add_squares(1, 2)
-#' add_squares(1, -2)
+#' myspotifymood("billybob08", "cbfj2895nbdudnejf2", "2kjgkjbrgrslenfjkrnjsne",
+#' "34IdsklfjbejQkbrsFG458HSf")
 #'
 #' @export
 
 # ESTABLISH CONNECTION SPOTIFY API
 
-my_spotify_mood = function(username = "lexapopee", client_id = "5b40eab3fe124c84a58aa5ed11ee6d0b",
-                           client_secret = "a1864c08c4cb4649a655278cab76d8a6"){
-  Sys.setenv(client_id)
-  Sys.setenv(client_secret)
-  access_token = get_spotify_access_token()
+my_spotify_mood = function(username, client_id, client_secret, playlists){
+  Sys.setenv(SPOTIFY_CLIENT_ID = client_id)
+  Sys.setenv(SPOTIFY_CLIENT_SECRET = client_secret)
+  access_token = spotifyr::get_spotify_access_token()
 
   # GET sPECIFIC PLAYLIST AUDIO FEATURES
 
-  playlists = spotifyr::get_user_playlists(x)
-  personal_playlist = spotifyr::get_playlist_audio_features("spotify:playlist:7IjYJeoglgPd0fLXQs6qtm" = p)
+  playlists = spotifyr::get_user_playlists(username)
+
+  p = playlists
 
   # GET PLAYLIST TRACKS VALENCE
 
-    playlist_track_valence = spotifyr::get_playlist_tracks(p)%>%
-    spotifyr::get_playlist_audio_features(p)%>%
-    group_by(track.name, valence)%>%
-    filter(valence >= 0.1)%>%
-    ggplot(aes(x = valence, y = track.name, fill = ..x..)) +
-    geom_col()
-
-  # GET PLAYLIST TRACKS POPULARITY
-
-  playlist_track_popularity = p %>%
+    playlist_track_valence = spotifyr::get_playlist_tracks(p) %>%
+    spotifyr::get_playlist_audio_features(p) %>%
     group_by(track.name, valence) %>%
-    filter(track.popularity >= 1) %>%
-    ggplot(aes(x = track.popularity, y = track.name, fill = ..x..)) +
+    filter(valence >= 0.1)
+
+    playlist_track_valence %>% ggplot(aes(x = valence, y = track.name, fill = ..x..)) +
     geom_col()
-
-  # GET TASTE QUADRANTS
-
-  TasteQuadrants <- ggplot(data = personal_playlist, aes(x = valence, y = track.popularity, color = track.explicit)) +
-    geom_jitter() +
-    geom_vline(xintercept = 0.5) +
-    geom_hline(yintercept = 0.5) +
-    scale_x_continuous(expand = c(0, 0), limits = c(0, 1)) +
-    scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +
-    annotate('text', 0.25 / 2, 0.95, label = "angry") +
-    annotate('text', 1.75 / 2, 0.95, label = "happy") +
-    annotate('text', 1.75 / 2, 0.05, label = "chill") +
-    annotate('text', 0.25 / 2, 0.05, label = "sad") +
-    labs(x= "valence", y= "popularity") +
-    ggtitle("Your Music Taste in Quadrants", "Defining your music taste based on a specifc Spotify Playlist")
-
 }
 
 
